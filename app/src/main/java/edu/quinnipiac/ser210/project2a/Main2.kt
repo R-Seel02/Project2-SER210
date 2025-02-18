6d2850a510b50c545d801205fdce8348904d3e88
R-Seel02 <Ryanjseely@icloud.com>
2025-02-17 21:28:04 -0500

    project2b

---
+++ app/src/main/java/edu/quinnipiac/ser210/project2a/Main2.kt
@@ -0,0 +1,81 @@
+/*
+Ryan Seely
+Feb 17 2025
+This project uses lazyColumn to make a list of countries, their flags and a fun fact.
+ */
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun CountryList(modifier: Modifier = Modifier
+) {
+    CountryListLColumn(countryList = CountryList, modifier)
+}
+
+
+
+@Composable
+fun CountryListLColumn(countryList:List<Country>,modifier: Modifier = Modifier  //creates the lazy column
+) {
+    LazyColumn(modifier=modifier) {
+        items(items= countryList){ country ->
+            CountryItem(country = country, modifier)
+
+        }
+    }
+}
+
+
+@Composable
+fun CountryItem(country: Country, modifier: Modifier = Modifier //all the components of the text and images
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .background(Color.Cyan)
+    ) {
+        Image(
+            painter = painterResource(id = country.flag),
+            contentDescription = "Flag of ${country.name}",
+            modifier = Modifier.size(50.dp).padding(4.dp)
+        )
+        Text(
+            text = country.name,
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier.padding(4.dp)
+        )
+        Text(
+            text = "Currency: ${country.currency}",
+            style = MaterialTheme.typography.bodySmall,
+            modifier = Modifier.padding(4.dp)
+        )
+        Text(
+            text = "Fun Fact: ${country.fact}",
+            style = MaterialTheme.typography.bodySmall,
+            modifier = Modifier.padding(4.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun PersonListPreview() {
+    CountryList()
+}
